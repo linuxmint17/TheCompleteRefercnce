d8a120fdaca582e746cb465ec6cfa576d0771f6b
suse <opensuselinux13.2@gmail.com>
2017-10-05 20:22:43 -0400

    don't konw what has been changed in overloadingFucntions.cpp

--- 14FuncitonOverloading/overloadingFuctions.cpp
+++ 14FuncitonOverloading/overloadingFuctions.cpp
@@ -5,7 +5,25 @@
 #include<cmath>
 #include<iomanip>
 using namespace std;
+int myfunc(int i);
+int myfunc(int i, int j);
+double myfunc(double i);
 int main(void)
 {
+  cout<<myfunc(10)<<" ";//calls myfunc(int i)
+  cout<<myfunc(55.4)<<endl;//calls myfunc(double i)
+  cout<<myfunc(4,5)<<endl;//calls myfunc(int i,int j)
   return 0;
+}
+double myfunc(double i)
+{
+  return i;
+}
+int myfunc(int i)
+{
+  return i;
+}
+int myfunc(int i,int j)
+{
+  return i*j;
 }
